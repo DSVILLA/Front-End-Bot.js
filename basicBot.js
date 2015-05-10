98ff5f5884277aa7af9ba083f910641bb691b19e
DSVILLA <equipevilla@live.com>
2015-05-10 00:45:11 -0400

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -183,7 +183,7 @@
         status: false,
         name: "basicBot",
         loggedInID: null,
-        scriptLink: "https://rawgit.com/B1G-B0SS/Exercito-Bot/master/basicBot.js",
+        scriptLink: "https://rawgit.com/DSVILLA/BotDS/master/basicBot.js",
         cmdLink: "http://git.io/245Ppg",
         chatLink: "https://rawgit.com/DSVILLA/BotDS/master/lang/pt.json",
         chat: null,
