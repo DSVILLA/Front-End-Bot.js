ff54ced33453e1f67a228a509576640c21efd93f
DSVILLA <equipevilla@live.com>
2015-05-10 00:14:46 -0400

    Create extension.js

---
+++ extension.js
@@ -0,0 +1,118 @@
+(function () {
+    //Link location of your fork so you don't have to modify so many things.
+    var fork = "Yemasthui";
+		
+    //Define our function responsible for extending the bot.
+    function extend() {
+        //If the bot hasn't been loaded properly, try again in 1 second(s).
+        if (!window.bot) {
+            return setTimeout(extend, 1 * 1000);
+        }
+
+        //Precaution to make sure it is assigned properly.
+        var bot = window.bot;
+
+        //Load custom settings set below
+        bot.retrieveSettings();
+
+        /*
+         Extend the bot here, either by calling another function or here directly.
+         Model code for a bot command:
+
+         bot.commands.commandCommand = {
+         command: 'cmd',
+         rank: 'user/bouncer/mod/manager',
+         type: 'startsWith/exact',
+         functionality: function(chat, cmd){
+         if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+         if( !bot.commands.executable(this.rank, chat) ) return void (0);
+         else{
+         //Commands functionality goes here.
+         }
+         }
+         }
+
+         */
+
+        bot.commands.zoeiraCommand = {
+            command: ['zoeira'],  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me O Viadão do editor não terminou esse comando vai ter que espera -_-´ ");
+                }
+            }
+        };
+
+        //Load the chat package again to account for any changes
+        bot.loadChat();
+
+    }
+
+    //Change the bots default settings and make sure they are loaded on launch
+
+    localStorage.setItem("basicBotsettings", JSON.stringify({
+        botName: "Comer Voces <3 BOT",
+        language: "portuguese",
+        startupCap: 20, // 1-200
+        startupVolume: 0, // 0-100
+        startupEmoji: true, // true or false
+        chatLink: "https://rawgit.com/DSVILLA/BotVIP/master/lang/pt",
+        maximumAfk: 60,			
+        afkRemoval: false,
+        maximumDc: 30,
+        bouncerPlus: true,
+        blacklistEnabled: true,
+        lockdownEnabled: false,
+        lockGuard: false,
+        maximumLocktime: 10,
+        cycleGuard: true,
+        maximumCycletime: 10,
+        voteSkip: true,
+        voteSkipLimit: 10,
+        timeGuard: true,
+        maximumSongLength: 06,
+        autodisable: false,
+        commandCooldown: 30,
+        usercommandsEnabled: true,
+        lockskipPosition: 1,
+        lockskipReasons: [
+            ["theme", "This song does not fit the room theme. "],
+            ["op", "This song is on the OP list. "],
+            ["history", "This song is in the history. "],
+            ["mix", "You played a mix, which is against the rules. "],
+            ["sound", "The song you played had bad sound quality or no sound. "],
+            ["nsfw", "The song you contained was NSFW (image or sound). "],
+            ["unavailable", "The song you played was not available for some users. "]
+        ],
+        afkpositionCheck: 15,
+        afkRankCheck: "ambassador",
+        motdEnabled: true,
+        motdInterval: 10,
+        motd: "!roulette",
+        filterChat: true,
+        etaRestriction: false,
+        welcome: true,
+        opLink: null,
+        rulesLink: " Mais pra frente adiciono as regras enquanto isso bons modos seus viadoes",
+        themeLink: true,
+        fbLink: true,
+        youtubeLink: true,
+        website: true,
+        intervalMessages: [],
+        messageInterval: 5,
+        songstats: false,
+        commandLiteral: "!",
+        blacklists: {
+            NSFW: "https://rawgit.com/" + fork + "/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
+            OP: "https://rawgit.com/" + fork + "/basicBot-customization/master/blacklists/ExampleOPlist.json"
+        }
+    }));
+
+    //Start the bot and extend it when it has loaded.
+    $.getScript("https://rawgit.com/DSVILLA/Exercito-Bot/master/basicBot.js", extend);
+
+}).call(this);
