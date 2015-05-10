1321b1bee96d2d0e5e941e0c2f8ae961c3d69e58
DSVILLA <equipevilla@live.com>
2015-05-10 01:14:59 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -113,6 +113,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/DSVILLA/BotDS/master/bot/basicBot.js", extend);
+    $.getScript("https://rawgit.com/DSVILLA/BotDS/master/bot1/basicBot.js", extend);
 
 }).call(this);
